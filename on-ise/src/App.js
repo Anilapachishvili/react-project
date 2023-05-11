c0b61bfe75c50434b5594c8ad8f2c9bd4393f79d
Ani <anilafachishvili2020@gmail.com>
2023-05-11 07:58:36 -0700

    change files

--- on-ise/src/App.js
+++ on-ise/src/App.js
@@ -1,22 +1,35 @@
 import "./App.css";
-import Header from './Components/Header';
-import Main from './Components/Main';
-import MainPage from "./Components/Mainpage";
-import Footer from "./Components/Footer";
+import Lists from './Components/Notification';
+import greenImage from "./images/green.png"
 
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Main/>
-      <MainPage/>
-      <Footer/>
-     
+    <div className="App">
 
-    
+     <div className='flex'>
+      <div className='Success'>
+
+       <Lists
+             imageSrc={greenImage}
+        title= "Success" 
+        text="Order Placed Successfully. You can check order delivery status."
+          />
+       </div>
+       
+      <div className='Error'> 
+      <Lists title ="Error" text="Order Placed Successfully. You can check order delivery status."/>
+      </div>
+
+      <div className='Warning'>
+       <Lists title ="Warning" text="Order Placed Successfully. You can check order delivery status."/>
+       </div>
+       <div className='Info'>
+       <Lists title ="Info" text="Order Placed Successfully. You can check order delivery status."/>
+       </div>
+       </div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ on-ise/src/Components/Notification.jsx
@@ -0,0 +1,18 @@
+import "./notification.css"
+
+function Lists(props) {
+
+  return (
+    <div>
+  
+      <h1 className="title">{props.title}</h1>
+       <p className="text">{props.text}</p>
+  
+  </div>
+  )
+
+  
+};
+
+export default Lists;
+
